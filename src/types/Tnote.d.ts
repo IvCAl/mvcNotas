c1f6f95e20ae9bb7fcc8455981bc35693e10d049
IvCAl <calivaivanalejandro@gmail.com>
2024-03-28 23:34:08 -0300

    Types creados

--- src/types/Tnote.d.ts
+++ src/types/Tnote.d.ts
@@ -1,18 +1,24 @@
-type Tnote={
-    id:number
-    name:string
-    description:string
-    resolve:boolean
+type FechaNota={
+    FechaCreate: Date
+    FechaUpdate: Date
 }
-
+type Detalle={
+    nombre: string
+    descripcion: string 
+    resolve: boolean
+}
+type Tnote={
+    id:string
+    detalles: Detalle
+    fecha:FechaNota
+    delete:boolean
+}
 function isString(str:any):boolean{
     return (typeof str === "string")
 }
-
 function isBoolean(bool: any):boolean{
     return typeof bool ==="boolean"
 }
-
 function parseString(string: any){
     if  (!isString(string)) throw new Error('Dato erroneo, no es un STRING')
     else return  string as string
@@ -21,21 +27,46 @@ function parseBoolean(bool:any){
     if ( !isBoolean(bool))throw new Error("Dato erroneo, no es un BOOLEAN")
     else return bool as boolean
 }
-
-export function  createNote(name:any,description:string):Tnote{
-    const note:Tnote
-    id=new Date(Date.now()).getTime();
+function createDate(): FechaNota{
+    const fecha:FechaNota
+    fech=new Date(Date.now()).getTime();
+    fecha.FechaCreate = fech
+    fecha.FechaUpdate = fech
+    return fecha
+}
+function createDetalle(nombre:string,descripcion:string):Detalle {
     nName=parseString(name)
     ndescription=parseString(description)
     resolve=false
-    return note={id,nName,nDescription,resolve}
+    const detalle:Detalle={nName,ndescription,resolve}
+    return detalle;
 }
-export function resolveNote(note:Tnote,data:any){
-    note.resolve=parseBoolean(data)
+function updateFecha(nota: Tnote) {
+    nota.fecha.FechaUpdate=new Date(Date.now()).getTime()
+} 
+export function createNota(id:string,nombre:string,descripcion:string):Tnote{
+    const fecha=createDate()
+    const detalle=createDetalle(nombre,descripcion)
+    nId=parseString(id)
+    const nota:Tnota={nId,detalle,fecha,delete:false}
+    return nota
 }
-export function changeDescription(note:Tnote,data:any){
-    note.description = parseString(data)
+
+
+export function resolveNota(nota:Tnote,resolve:boolean){
+    nota.detalles.resolve=parseBoolean(resolve)
+    updateFecha(nota)
 }
-export function changeName(note:Tnote, data:any) {
-    note.description= parseString(data)
+
+export function changeDescripcion(nota:Tnote,descripcion:string){
+    nota.detalles.descripcion = parseString(descripcion)
+    updateFecha(nota)
+}
+export function changeNombre(nota:Tnote, nombre:string) {
+    nota.detalles.nombre= parseString(nombre)
+    updateFecha(nota)
+}
+export function deleteNota(nota:Tnote){
+    nota.delete=true
+    updateFecha(nota)
 }
